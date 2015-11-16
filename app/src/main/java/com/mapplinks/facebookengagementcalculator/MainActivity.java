b15e2c44e56cea34ed29d9771a82b1ca909a820d
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2015-11-16 20:11:45 +0530

    New Layout

--- app/src/main/java/com/mapplinks/facebookengagementcalculator/MainActivity.java
+++ app/src/main/java/com/mapplinks/facebookengagementcalculator/MainActivity.java
@@ -1,14 +1,23 @@
 package com.mapplinks.facebookengagementcalculator;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Long likes, comments, shares, postReach, pageLikes;
+    Double likes, comments, shares, postReach, pageLikes, result;
+    String stLikes, stComments, stShare, stPostReach, stPageLikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +26,129 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final EditText postLikesNumber = (EditText) findViewById(R.id.likes);
+        final EditText postCommentsNumber = (EditText) findViewById(R.id.comments);
+        final EditText postSharesNumber = (EditText) findViewById(R.id.shares);
+        final TextView postResult = (TextView) findViewById(R.id.result);
+        final EditText postReachNumber = (EditText) findViewById(R.id.reach);
+        final EditText pageLikeNumber = (EditText) findViewById(R.id.page_likes);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postLikesNumber.setText("");
+                postCommentsNumber.setText("");
+                postSharesNumber.setText("");
+                postResult.setText("");
+                postReachNumber.setText("");
+                pageLikeNumber.setText("");
+
+                postReachNumber.setEnabled(false);
+                pageLikeNumber.setEnabled(false);
+
+                postLikesNumber.requestFocus();
+            }
+        });
+
+
+        Button byReach = (Button) findViewById(R.id.byReach);                       //Engagement by Reach
+        byReach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stLikes = postLikesNumber.getText().toString();
+                stComments = postCommentsNumber.getText().toString();
+                stShare = postSharesNumber.getText().toString();
+
+                if (stLikes.isEmpty() || stComments.isEmpty() || stComments.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter all the three values", Toast.LENGTH_SHORT).show();
+                } else {
+                    postReachNumber.setEnabled(true);
+                    postReachNumber.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            stPostReach = postReachNumber.getText().toString();
+                            if(stPostReach.isEmpty()){
+                                Toast.makeText(MainActivity.this, "Enter a valid number", Toast.LENGTH_SHORT).show();
+                            }else{
+                                postReach = Double.parseDouble(stPostReach);
+                                shares = Double.parseDouble(stShare);
+                                comments = Double.parseDouble(stComments);
+                                likes = Double.parseDouble(stLikes);
+                                result = reachEngagement();
+                                postResult.setText("Reach by Engagement: " + result);
+
+                            }
+                        }
+                    });
+                }
+            }
+
+        });
+
+//----------------------------------------------------------------------------------------------------------------------------//
+        Button byFans = (Button) findViewById(R.id.byFans);
+        byFans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stLikes = postLikesNumber.getText().toString();
+                stComments = postCommentsNumber.getText().toString();
+                stShare = postSharesNumber.getText().toString();
+
+                if (stLikes.isEmpty() || stComments.isEmpty() || stComments.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter all the three values", Toast.LENGTH_SHORT).show();
+                } else {
+                    pageLikeNumber.setEnabled(true);
+                    pageLikeNumber.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            stPageLikes = pageLikeNumber.getText().toString();
+                            if(stPageLikes.isEmpty()){
+                                Toast.makeText(MainActivity.this, "Enter a valid number", Toast.LENGTH_SHORT).show();
+                            }else{
+                                pageLikes = Double.parseDouble(stPageLikes);
+                                shares = Double.parseDouble(stShare);
+                                comments = Double.parseDouble(stComments);
+                                likes = Double.parseDouble(stLikes);
+                                result = reachFans();
+                                postResult.setText("Reach by Fans: " + result);
+
+                            }
+                        }
+                    });
+                }
+            }
+
+        });
     }
 
-    double reachEngagement(){
-        return (((likes + comments + shares)/postReach)*100);
+    double reachEngagement() {
+        return (((likes + comments + shares) / postReach) * 100);
 
     }
-    double reachFans(){
-        return (((likes + comments + shares)/pageLikes)*100);
+
+    double reachFans() {
+        return (((likes + comments + shares) / pageLikes) * 100);
 
     }
 
@@ -55,7 +170,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
